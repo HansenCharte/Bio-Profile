3e9f8afa3eef73e12f6500b0a1dd13ef32cf50c5
HansenCharte <100987760+HansenCharte@users.noreply.github.com>
2022-03-28 22:23:29 +0700

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter_windows_2.10.3-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Hansen\\Documents\\Draw IO\\bio_profile-master_2\\bio_profile-master" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter_windows_2.10.3-stable\\flutter"
+  "PROJECT_DIR=C:\\Users\\Hansen\\Documents\\Draw IO\\bio_profile-master_2\\bio_profile-master"
+  "FLUTTER_ROOT=C:\\flutter_windows_2.10.3-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Hansen\\Documents\\Draw IO\\bio_profile-master_2\\bio_profile-master\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Hansen\\Documents\\Draw IO\\bio_profile-master_2\\bio_profile-master"
+  "FLUTTER_TARGET=C:\\Users\\Hansen\\Documents\\Draw IO\\bio_profile-master_2\\bio_profile-master\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Hansen\\Documents\\Draw IO\\bio_profile-master_2\\bio_profile-master\\.dart_tool\\package_config.json"
+)
